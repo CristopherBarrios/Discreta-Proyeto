ca17542c7a01513be883e3d1471dda5d54d6dc80
Darlight <andres.perdomo09@gmail.com>
2019-11-19 13:16:59 -0600

    Comparando código respecto a tiempo de ejecución

--- Particiones.py
+++ Particiones.py
@@ -1,23 +1,25 @@
 """
 Univesidad del valle de Guatemala
-Seccion xx - MM2015
+Seccion 10 - MM2015
 Matematica discreta
 
 Mario Perdomo 18029
 Josue Sagastume 18173
-Christopher Barrios 181xx
+Christopher Barrios 18207
 Proyecto 1: Particion de un entero
 """
 
 #Son todas las posibles respuestas respecto al numkero 'k'
 particiones = [] 
 num = 0
-particiones = __gen_particiones(num)
+particiones = gen_particiones(num)
 def print_particiones(particiones):
     # Es lo que separa las respuesta
     string_particiones = ''
     for serie in particiones:
         #Muestra la n cantidad de respuestas que sumados dan el numero "k"
+
+        #5 = 1 + 1 + 3
         string_particiones += str(num) + ' = ' + ' + '.join([str(num) for num in serie]) + '\n'
         print (string_particiones)
  
@@ -40,3 +42,43 @@ def gen_particiones(particiones, num, limit = False):
                 for i in gen_particiones(num - x, x):
                     i.append(x)
                     yield i
+
+
+"""
+ public class IntegerPartition
+{
+    public static int[,] Result = new int[100,100];
+
+    private static int Partition(int targetNumber, int largestNumber)
+    {
+        for (int i = 1; i <= targetNumber; i++)
+        {
+            for (int j = 1; j <= largestNumber; j++)
+            {
+                if (i - j < 0)
+                {
+                    Result[i, j] = Result[i, j - 1];
+                    continue;
+                }
+                Result[i, j] = Result[i, j - 1] + Result[i - j, j];
+            }
+        }
+        return Result[targetNumber, largestNumber];
+    }
+
+    public static int Main(int number, int target)
+    {
+        int i;
+        for (i = 0; i <= number; i++)
+        {
+            Result[i, 0] = 0;
+        }
+        for (i = 1; i <= target; i++)
+        {
+            Result[0, i] = 1;
+        }
+        return Partition(number, target);
+    }
+}
+
+"""
